44f8362492c1ccebea553db041a0c8ab238e0041
Matias <matiassarrailh@gmail.com>
2024-06-08 01:48:40 +0200

    Exercise correction with proper string formatting output

--- passwordChecker.py
+++ passwordChecker.py
@@ -2,7 +2,7 @@
 
 def check_password(username, password):
     hidden_password = '*' * len(password)
-    print(hidden_password)
+    print(f'Hey {username}, your password {hidden_password} is {len(password)} letters long.')
 
 
 check_password("username", "password")
--- passwordChecker_test.py
+++ passwordChecker_test.py
@@ -8,7 +8,7 @@ class TestPasswordChecker(unittest.TestCase):
     @patch('sys.stdout', new_callable=StringIO)
     def test_password_checker(self, mock_stdout):
         check_password("username", "password")
-        self.assertEqual(mock_stdout.getvalue().strip(), "********")
+        self.assertEqual(mock_stdout.getvalue().strip(), "Hey username, your password ******** is 8 letters long.")
 
 
 if __name__ == '__main__':
