cb6a38fad0638b2668275b58b99fe7dfc0cfcfc7
Matias <matiassarrailh@gmail.com>
2024-06-13 15:18:13 +0200

    Pets exercise 4 Pillars of Programming

---
+++ pets.py
@@ -0,0 +1,50 @@
+class Pets():
+    animals = []
+
+    def __init__(self, animals):
+        self.animals = animals
+
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
+
+
+class Cat():
+    is_lazy = True
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def walk(self):
+        return f'{self.name} is just walking around'
+
+
+class Simon(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+
+class Sally(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+
+# 1 Add nother Cat
+class Peluca(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+
+# 2 Create a list of all of the pets (create 3 cat instances from the above)
+cat1 = Simon('Simon', 15)
+cat2 = Sally('Sally', 14)
+cat3 = Peluca('Peluca', 19)
+
+my_cats = [cat1, cat2, cat3]
+
+# 3 Instantiate the Pet class with all your cats use variable my_pets
+my_pets = Pets(my_cats)
+# 4 Output all of the cats walking using the my_pets instance
+for pet in my_pets.animals:
+    print(pet.walk())
